3ca8b3231de8b78b73272790e41769409c09b1fe
Banoth Charan <charancherry1129@gmail.com>
2026-02-14 23:15:41 +0530

    feat: integrate Footer component to App layout

--- src/App.tsx
+++ src/App.tsx
@@ -1,12 +1,16 @@
 import React from "react";
 import Hero from "./components/Hero";
 import About from "./components/About";
+import Footer from "./components/Footer";
 
 const App: React.FC = () => {
   return (
-    <main className="w-full min-h-screen bg-night-dark text-white overflow-x-hidden">
-      <Hero />
-      <About />
+    <main className="w-full min-h-screen bg-night-dark text-white overflow-x-hidden flex flex-col">
+      <section className="flex-1">
+        <Hero />
+        <About />
+      </section>
+      <Footer />
     </main>
   );
 };
